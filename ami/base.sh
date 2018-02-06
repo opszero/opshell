d497ebf8890e5aa9b0f7e1862370ef97c98976ff
Kyle1668 <kyledevinobrien1@gmail.com>
2018-02-06 10:01:56 -0800

    added golang install

--- ami/base.sh
+++ ami/base.sh
@@ -27,6 +27,18 @@ sudo apt-get install -y apt-transport-https \
                         wget \
                         zsh
 
+
+# Go
+echo 'golang installation begin'
+wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
+sudo tar -xvf go1.9.2.linux-amd64.tar.gz
+sudo mv go /usr/local
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/Projects/Proj1
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+go version
+echo 'golang installation end'
+
 sudo pip install --upgrade pip
 
 # AWS
