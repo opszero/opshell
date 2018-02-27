132f5242ef5b56ebedcad164820045dacb584828
Kyle1668 <kyledevinobrien1@gmail.com>
2018-02-27 05:18:12 +0000

    initial commit

--- ami/base.sh
+++ ami/base.sh
@@ -27,6 +27,17 @@ sudo apt-get install -y apt-transport-https \
                         wget \
                         zsh
 
+# Ruby
+echo 'ruby begin_________________________________________________'
+sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDBi -y
+sudo apt-get install software-properties-common -y 
+sudo apt-add-repository -y ppa:rael-gc/rvm -y
+sudo apt-get update -y
+sudo apt-get install rvm -y 
+sudo rvm install ruby --latest
+ruby -v
+echo 'ruby end-_-_-_-_-_-_-_-_-'
+echo '____________________________________________ \n\n]n'
 
 # Go
 wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
